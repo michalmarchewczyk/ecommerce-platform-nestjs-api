b94679e6ac50049b26a6540ac4c56c0eb609d39d
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-08 01:15:32 +0200

    Configure TypeORM and PostgreSQL
    Signed-off-by: Michał Marchewczyk <dev@marchewczyk.eu>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'ecommerce-platform',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

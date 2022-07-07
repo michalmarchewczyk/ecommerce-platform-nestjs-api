ccd942ec2c1929a3986152d12b657584b4a59e6f
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-08 00:48:00 +0200

    Change default app port
    Signed-off-by: Michał Marchewczyk <dev@marchewczyk.eu>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
 bootstrap();

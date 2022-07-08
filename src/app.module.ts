bcb946f6c8ff96356fada262fb4aacd40d28f536
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-08 15:32:51 +0200

    Setup tests
    Signed-off-by: Michał Marchewczyk <dev@marchewczyk.eu>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
@@ -18,6 +17,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}

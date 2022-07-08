d3617b31807249b904c0d130c55d79691fc157ed
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-09 00:35:43 +0200

    Setup OpenAPI
    Signed-off-by: Michał Marchewczyk <dev@marchewczyk.eu>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce platform API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 }

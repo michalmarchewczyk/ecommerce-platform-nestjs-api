5d66434fd7eb4665a2f173a1e8fc245a8b920b5c
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-24 12:19:36 +0200

    Setup Redoc API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { RedocModule } from 'nestjs-redoc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
     .setVersion(process.env.npm_package_version ?? '0.0.2')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document);
+  await RedocModule.setup('docs', app, document, {});
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');

c2a197968457b437dc6c79daffaff4e7045477ad
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-26 23:30:34 +0200

    Add api descriptions to product ratings controller

--- src/products/product-ratings/product-ratings.controller.ts
+++ src/products/product-ratings/product-ratings.controller.ts
@@ -16,12 +16,27 @@ import { Roles } from '../../auth/roles.decorator';
 import { Role } from '../../users/entities/role.enum';
 import { ReqUser } from '../../auth/user.decorator';
 import { User } from '../../users/entities/user.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('product ratings')
 @Controller('product-ratings')
 export class ProductRatingsController {
   constructor(private readonly productRatingsService: ProductRatingsService) {}
 
   @Get(':productId')
+  @ApiNotFoundResponse({ description: 'Product not found' })
+  @ApiOkResponse({
+    type: [ProductRating],
+    description: 'List or ratings for given product',
+  })
   async getProductRatings(
     @Param('productId', ParseIntPipe) productId: number,
   ): Promise<ProductRating[]> {
@@ -30,6 +45,11 @@ export class ProductRatingsController {
 
   @Post(':productId')
   @Roles(Role.Admin, Role.Manager, Role.Sales, Role.Customer)
+  @ApiNotFoundResponse({ description: 'Product not found' })
+  @ApiUnauthorizedResponse({ description: 'User not logged in' })
+  @ApiForbiddenResponse({ description: 'User not authorized' })
+  @ApiCreatedResponse({ description: 'Product rating created' })
+  @ApiBadRequestResponse({ description: 'Invalid rating data' })
   async createProductRating(
     @ReqUser() user: User,
     @Param('productId', ParseIntPipe) productId: number,
@@ -44,6 +64,11 @@ export class ProductRatingsController {
 
   @Put(':productId/:id')
   @Roles(Role.Admin, Role.Manager, Role.Sales, Role.Customer)
+  @ApiUnauthorizedResponse({ description: 'User not logged in' })
+  @ApiForbiddenResponse({ description: 'User not authorized' })
+  @ApiNotFoundResponse({ description: 'Product rating not found' })
+  @ApiOkResponse({ description: 'Product rating updated' })
+  @ApiBadRequestResponse({ description: 'Invalid rating data' })
   async updateProductRating(
     @ReqUser() user: User,
     @Param('productId', ParseIntPipe) productId: number,
@@ -66,6 +91,10 @@ export class ProductRatingsController {
 
   @Delete(':productId/:id')
   @Roles(Role.Admin, Role.Manager, Role.Sales, Role.Customer)
+  @ApiUnauthorizedResponse({ description: 'User not logged in' })
+  @ApiForbiddenResponse({ description: 'User not authorized' })
+  @ApiNotFoundResponse({ description: 'Product rating not found' })
+  @ApiOkResponse({ description: 'Product rating deleted' })
   async deleteProductRating(
     @ReqUser() user: User,
     @Param('productId', ParseIntPipe) productId: number,

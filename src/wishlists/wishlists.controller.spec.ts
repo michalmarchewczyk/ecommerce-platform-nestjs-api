b16d59c2e26023b4891ed3decf24a71a78e30b7b
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-27 22:03:43 +0200

    Add wishlist management unit tests

---
+++ src/wishlists/wishlists.controller.spec.ts
@@ -0,0 +1,150 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { WishlistsController } from './wishlists.controller';
+import { WishlistsService } from './wishlists.service';
+import { RepositoryMockService } from '../../test/utils/repository-mock/repository-mock.service';
+import { Wishlist } from './entities/wishlist.entity';
+import { Product } from '../products/entities/product.entity';
+import { DtoGeneratorService } from '../../test/utils/dto-generator/dto-generator.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ProductCreateDto } from '../products/dto/product-create.dto';
+import { WishlistCreateDto } from './dto/wishlist-create.dto';
+import { User } from '../users/entities/user.entity';
+import { NotFoundError } from '../errors/not-found.error';
+
+describe('WishlistsController', () => {
+  let controller: WishlistsController;
+  let generate: DtoGeneratorService['generate'];
+  let mockWishlistsRepository: RepositoryMockService<Wishlist>;
+  let mockProductsRepository: RepositoryMockService<Product>;
+  let testProduct: Product;
+  let testWishlist: Wishlist;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [WishlistsController],
+      providers: [
+        WishlistsService,
+        RepositoryMockService.getProvider(Wishlist),
+        RepositoryMockService.getProvider(Product),
+        DtoGeneratorService,
+      ],
+    }).compile();
+
+    controller = module.get<WishlistsController>(WishlistsController);
+
+    generate = module
+      .get<DtoGeneratorService>(DtoGeneratorService)
+      .generate.bind(module.get<DtoGeneratorService>(DtoGeneratorService));
+    mockWishlistsRepository = module.get(getRepositoryToken(Wishlist));
+    mockProductsRepository = module.get(getRepositoryToken(Product));
+
+    const productData = generate(ProductCreateDto);
+    testProduct = await mockProductsRepository.save(productData);
+
+    const createData = generate(WishlistCreateDto);
+    testWishlist = await mockWishlistsRepository.save({
+      ...createData,
+      user: { id: 123 },
+      products: [testProduct],
+    });
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getUserWishlists', () => {
+    it('should return user wishlists', async () => {
+      const wishlists = await controller.getUserWishlists({ id: 123 } as User);
+      expect(wishlists).toContainEqual(testWishlist);
+    });
+
+    it("shouldn't return other user wishlists", async () => {
+      const wishlists = await controller.getUserWishlists({
+        id: 12345,
+      } as User);
+      expect(wishlists).toEqual([]);
+    });
+  });
+
+  describe('createWishlist', () => {
+    it('should create wishlist', async () => {
+      const createData = generate(WishlistCreateDto);
+      createData.productIds = [testProduct.id];
+      const wishlist = await controller.createWishlist(
+        { id: 123 } as User,
+        createData,
+      );
+      expect(wishlist).toMatchObject({ name: createData.name });
+    });
+
+    it('should return error if product not found', async () => {
+      const createData = generate(WishlistCreateDto);
+      createData.productIds = [12345];
+      await expect(
+        controller.createWishlist({ id: 123 } as User, createData),
+      ).rejects.toThrow(NotFoundError);
+    });
+  });
+
+  describe('updateWishlist', () => {
+    it('should update wishlist', async () => {
+      const createData = generate(WishlistCreateDto);
+      createData.productIds = [];
+      const { id } = await controller.createWishlist(
+        { id: 123 } as User,
+        createData,
+      );
+      const updateData = generate(WishlistCreateDto, true);
+      updateData.productIds = [testProduct.id];
+      const wishlist = await controller.updateWishlist(
+        { id: 123 } as User,
+        id,
+        updateData,
+      );
+      expect(wishlist).toMatchObject({ name: updateData.name });
+    });
+
+    it('should return error if product not found', async () => {
+      const createData = generate(WishlistCreateDto);
+      createData.productIds = [];
+      const { id } = await controller.createWishlist(
+        { id: 123 } as User,
+        createData,
+      );
+      const updateData = generate(WishlistCreateDto, true);
+      updateData.productIds = [12345];
+      await expect(
+        controller.updateWishlist({ id: 123 } as User, id, updateData),
+      ).rejects.toThrow(NotFoundError);
+    });
+
+    it('should return error if wishlist not found', async () => {
+      const updateData = generate(WishlistCreateDto, true);
+      await expect(
+        controller.updateWishlist({ id: 123 } as User, 12345, updateData),
+      ).rejects.toThrow(NotFoundError);
+    });
+  });
+
+  describe('deleteWishlist', () => {
+    it('should delete wishlist', async () => {
+      const createData = generate(WishlistCreateDto);
+      createData.productIds = [];
+      const { id } = await controller.createWishlist(
+        { id: 123 } as User,
+        createData,
+      );
+      await controller.deleteWishlist({ id: 123 } as User, id);
+      expect(
+        mockWishlistsRepository.entities.find((w) => w.id === id),
+      ).toBeUndefined();
+    });
+
+    it('should return error if wishlist not found', async () => {
+      await expect(
+        controller.deleteWishlist({ id: 123 } as User, 12345),
+      ).rejects.toThrow(NotFoundError);
+    });
+  });
+});

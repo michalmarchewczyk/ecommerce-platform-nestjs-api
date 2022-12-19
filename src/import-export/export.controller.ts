e3d3b89ec9ca706a26ab30506f6b8cdc54b5d716
Michał Marchewczyk <dev@marchewczyk.eu>
2022-12-19 17:14:53 +0100

    Use better export filename generation

--- src/import-export/export.controller.ts
+++ src/import-export/export.controller.ts
@@ -31,13 +31,12 @@ export class ExportController {
     @Res({ passthrough: true }) res: Response,
     @Body() data: ExportDto,
   ) {
-    const ext = data.format === 'csv' ? 'tar.gz' : 'json';
     const contentType =
       data.format === 'csv' ? 'application/gzip' : 'application/json';
     res.header('Content-Type', contentType);
     res.header(
       'Content-Disposition',
-      `attachment; filename="export-${new Date().toISOString()}.${ext}"`,
+      `attachment; filename="${this.exportService.getFilename(data.format)}"`,
     );
     res.header('Access-Control-Expose-Headers', 'Content-Disposition');
     return await this.exportService.export(data.data, data.format);
--- src/import-export/export.service.ts
+++ src/import-export/export.service.ts
@@ -52,6 +52,18 @@ export class ExportService {
     private returnsExporter: ReturnsExporter,
   ) {}
 
+  getFilename(format: 'json' | 'csv') {
+    const ext = format === 'csv' ? 'tar.gz' : 'json';
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    return `export_${year}${month}${day}_${hours}${minutes}${seconds}.${ext}`;
+  }
+
   async export(data: DataType[], format: 'json' | 'csv') {
     checkDataTypeDependencies(data);
     const toExport: Record<string, any[]> = {};

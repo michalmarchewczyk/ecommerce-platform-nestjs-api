d5f337318794821a579693e12a6df443c1b7a3a7
Michał Marchewczyk <dev@marchewczyk.eu>
2023-01-07 19:52:45 +0100

    Update cart model

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -4,9 +4,10 @@ import { CartsController } from './carts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cart } from './models/cart.entity';
 import { CatalogModule } from '../catalog/catalog.module';
+import { CartItem } from './models/cart-item.entity';
 
 @Module({
-  imports: [CatalogModule, TypeOrmModule.forFeature([Cart])],
+  imports: [CatalogModule, TypeOrmModule.forFeature([Cart, CartItem])],
   providers: [CartsService],
   controllers: [CartsController],
 })
--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { ProductsService } from '../catalog/products/products.service';
 import { User } from '../users/models/user.entity';
 import { CartDto } from './dto/cart.dto';
+import { CartItem } from './models/cart-item.entity';
 
 @Injectable()
 export class CartsService {
@@ -34,22 +35,25 @@ export class CartsService {
     if (user) {
       const cart = new Cart();
       cart.user = user;
-      cart.products = [];
+      cart.items = [];
       return await this.cartsRepository.save(cart);
     } else {
       const cart = new Cart();
       cart.sessionId = sessionId;
-      cart.products = [];
+      cart.items = [];
       return await this.cartsRepository.save(cart);
     }
   }
 
   async updateCart(updateData: CartDto, user: User | null, sessionId?: string) {
     const cart = await this.getCart(user, sessionId);
-    cart.products = [];
-    for (const productId of updateData.productIds) {
+    cart.items = [];
+    for (const { productId, quantity } of updateData.items) {
       const product = await this.productsService.getProduct(productId);
-      cart.products.push(product);
+      const item = new CartItem();
+      item.product = product;
+      item.quantity = quantity;
+      cart.items.push(item);
     }
     return await this.cartsRepository.save(cart);
   }
